4f21e1e0b1b86075722715e0d83272ddf77aa84a
Marco Lee <marcole32k3@gmail.com>
2026-02-26 16:27:13 -0800

    build vercel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import MainNavBar from "./components/Navigation/MainNavBar";
+import MainNavBar from "./components/Navigation/MainNavbar";
 import {Outlet} from 'react-router-dom';
 
 function App() {
